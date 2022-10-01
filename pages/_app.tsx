f51ecf0254dbf5e28aeac24ea286bacb9ec82991
Shadow <william.shadow@outlook.com>
2022-10-01 06:46:49 +0000

    login stuffs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,22 @@
-import '../styles/globals.css';
-import { NextUIProvider } from '@nextui-org/react';
-import { AppProps } from 'next/app';
+import "../styles/globals.css"
+import { NextUIProvider } from "@nextui-org/react"
+import { AppProps } from "next/app"
+import { Session } from "next-auth"
+import { SessionProvider } from "next-auth/react"
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <NextUIProvider>
-      <Component {...pageProps} />
-    </NextUIProvider>
-  );
+const App = ({
+    Component,
+    pageProps: { session, ...pageProps },
+}: AppProps<{
+    session: Session
+}>) => {
+    return (
+        <NextUIProvider>
+            <SessionProvider session={session}>
+                <Component {...pageProps} />
+            </SessionProvider>
+        </NextUIProvider>
+    )
 }
 
-export default MyApp;
+export default App
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,20 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
+import type { NextPage } from "next"
+import Head from "next/head"
+import LoginButton from "../components/loginBtn"
 
 const Home: NextPage = () => {
-  return (
-    <div>
-      <Head>
-        <title>Discord Match</title>
-      </Head>
-      <main>
-        <h1>Coming soon to a web near you</h1>
-      </main>
-      </div>
-  )
+    return (
+        <div>
+            <Head>
+                <title>Discord Match</title>
+            </Head>
+            <main>
+                <h1>Coming soon to a web near you</h1>
+            </main>
+
+            <LoginButton />
+        </div>
+    )
 }
 
 export default Home
