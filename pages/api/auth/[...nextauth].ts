7ec921803cae135e82e9e89fc6f1cf635cd951a6
Shadow <william.shadow@outlook.com>
2022-10-01 17:48:37 +0000

    hmmmm

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,11 +1,17 @@
-import NextAuth from "next-auth"
+import NextAuth, { Account, NextAuthOptions, User } from "next-auth"
 import DiscordProvider from "next-auth/providers/discord"
-export const authOptions = {
+export const authOptions: NextAuthOptions = {
     providers: [
         DiscordProvider({
             clientId: process.env.DISCORD_CLIENT_ID!,
             clientSecret: process.env.DISCORD_CLIENT_SECRET!,
         }),
     ],
+    callbacks: {
+        async signIn({ user, account, profile, email, credentials }) {
+            console.log(user, account, profile, email, credentials)
+            return true
+        },
+    },
 }
 export default NextAuth(authOptions)
---
+++ pages/api/user/me.ts
@@ -0,0 +1,22 @@
+import { NextApiRequest, NextApiResponse } from "next"
+import { User } from "next-auth"
+import { unstable_getServerSession } from "next-auth/next"
+import { APIError } from "../../../types"
+import { authOptions } from "../auth/[...nextauth]"
+
+const handler = async (req: NextApiRequest, res: NextApiResponse<User | APIError>) => {
+    const session = await unstable_getServerSession(req, res, authOptions)
+    if (!session || !session?.user) {
+        res.status(401).send({
+            message: "You must be signed in to view the protected content on this page.",
+        })
+    }
+
+	const user = await prisma.user.findUnique({
+		where: {
+			email: `${session!.user!.email}`,
+		},
+	})
+}
+
+export default handler
