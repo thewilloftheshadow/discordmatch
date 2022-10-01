8bbba0dcbaad69ba7e2d72fa907f6f6238cc1d5d
Shadow <william.shadow@outlook.com>
2022-10-01 04:51:59 +0000

    init

---
+++ pages/_document.tsx
@@ -0,0 +1,16 @@
+import { Html, Head, Main, NextScript } from "next/document"
+
+export default function Document() {
+    return (
+        <Html>
+            <Head>
+                <meta name="title" content="Reservoir Academic Classes" />
+                <link rel="icon" href="/logo.png" />
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    )
+}
---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Discord Match</title>
+      </Head>
+      <main>
+        <h1>Coming soon to a web near you</h1>
+      </main>
+      </div>
+  )
+}
+
+export default Home
